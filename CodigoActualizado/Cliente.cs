2ffe53186f7c1f6f4abb9b9ce7a9ae92ce56346b
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-12-19 09:58:12 -0300

    Create Cliente.cs

---
+++ CodigoActualizado/Cliente.cs
@@ -0,0 +1,13 @@
+using System;
+using utilidades;
+namespace VideoClub
+{
+    class Cliente : Detalles
+    {
+        public Cliente() : base() {
+        }
+        public string MostrarInformacionCliente(){
+            return $"Informacion del Cliente \nNombre: {Nombre} \nApellido: {Apellido} \nRut: {RUT} \nCorreo: {Correo} \nForma de pago: {FormadePago} ";
+        }
+    }
+}
